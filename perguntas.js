04276b474e3ec67300580dadb66ad727ff72c5bd
vickvaporu6 <00001064285077sp@al.educacao.sp.gov.br>
2024-11-21 12:06:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'Complete',
+    'Theres snow falling over the city You thought',
+    ' that it would wash away'
+)
+
+criaCartao(
+    'complete',
+    'I ve made some real big mistakes But you make the worst...',
+    'one look fine'
+)
+
+criaCartao(
+   'OLIVIA RODRIGO',
+    'Qual é os dois Album dela?',
+    'SOUR & GUTS '
+)
+
+criaCartao(
+'OLIVIA RODRIGO',
+    'Onde ela nasceu?',
+    'Murrieta, Califórnia, EUA'
+)
+
+criaCartao(
+    'OLIVIA RODRIGO',
+'A data de nascimento da Olivia roodrigo?',
+'20 /02/2003'
+
+)
+
+criaCartao(
+'It girls',
+'As maiores it girls da geração z',
+' Olivia rodrigo, Billie Eillish, Bella hadid, Kylie jenner e Gigi hadid'
+
+)
+
+criaCartao(
+'MÚSICA',
+'Qual é o albúm mais escutado do spotfy',
+'Sour by Olivia rodrigo'
+
+)
+
+criaCartao(
+'MÚSICA',
+'Qual foi o primeiro EP do the weenked',
+'House of Balloons'
+
+)
+
+criaCartao(
+'Música',
+'Qual foi a artista que ganhou mais grammy em 2020',
+'Billie Eillish'
+
+)
